bb3570c7ee59a2bf25ab06da4ea8d44c32b1f2a0
Raj-Chhadia <raj.chhadia@gmail.com>
2022-12-28 20:28:24 +0530

    Initial commit

---
+++ DEMO/Grade/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Grade checker ==========");
+            int[] marks = new int[5];
+            int number = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter the subject " + (i+1) + " marks");
+                marks[i] = int.Parse(Console.ReadLine());
+                number += marks[i];
+
+            }
+            float percent = (number / 5);
+            Console.WriteLine("Your total marks is " + number);
+            Console.WriteLine("Your percentage is " + percent + "%");
+
+            
+            if (percent <  33)
+                Console.WriteLine("ypu are failed");
+            else if( percent < 70 )
+                Console.WriteLine("You are average");
+            else
+                Console.WriteLine("You are Awesome");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DEMO/Prime/Prime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to check");
+            int number = int.Parse(Console.ReadLine());
+            for(int i =2; i<number;i++)
+            {
+                if (number % i == 0)
+                    Console.WriteLine("Number is not prime");    
+                else
+                    Console.WriteLine("Number is prime");
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
